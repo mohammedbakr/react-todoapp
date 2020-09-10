57e47c8aaca971fcb074ab3ff01d1f733aa2bbb5
Mohamed Bakr <bakr.engineer@gmail.com>
2020-09-10 19:54:58 +0200

    Adding react-router-dom for home & about pages

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header style={headerStyle}>
       <h1>Todo List</h1>
+      <Link style={linkStyle} to="/">Home</Link> | <Link style={linkStyle} to="/about">About</Link>
     </header>
   )
 }
@@ -13,6 +15,10 @@ const headerStyle = {
   color: '#fff',
   padding: '10px',
   textAlign: 'center'
+}
+
+const linkStyle = {
+  color: '#fff'
 }
 
 export default Header;
